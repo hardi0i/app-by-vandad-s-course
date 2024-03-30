a094110f8262073b5a3ab1daf433a013a4d140f5
Dmitriy Kharitonov <howimanyitears@gmail.com>
2024-03-30 12:39:13 +0300

    change local storage to cloud

--- lib/views/notes/create_update_note_view.dart
+++ lib/views/notes/create_update_note_view.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:vandad_course_app/services/auth/auth_service.dart';
-import 'package:vandad_course_app/services/crud/notes_service.dart';
+import 'package:vandad_course_app/services/cloud/cloud_note.dart';
 import 'package:vandad_course_app/utilities/generics/get_arguments.dart';
+import 'package:vandad_course_app/services/cloud/firebase_cloud_storage.dart';
 
 class CreateUpdateNoteView extends StatefulWidget {
   const CreateUpdateNoteView({super.key});
@@ -11,16 +12,16 @@ class CreateUpdateNoteView extends StatefulWidget {
 }
 
 class _CreateUpdateNoteViewState extends State<CreateUpdateNoteView> {
-  late final NotesService _notesService;
+  late final FirebaseCloudStorage _notesService;
   late final TextEditingController _textController;
 
-  DatabaseNote? _note;
+  CloudNote? _note;
 
   @override
   void initState() {
     super.initState();
 
-    _notesService = NotesService();
+    _notesService = FirebaseCloudStorage();
     _textController = TextEditingController();
   }
 
@@ -31,7 +32,7 @@ class _CreateUpdateNoteViewState extends State<CreateUpdateNoteView> {
 
     final text = _textController.text;
     await _notesService.updateNote(
-      note: note,
+      documentId: note.documentId,
       text: text,
     );
   }
@@ -42,8 +43,8 @@ class _CreateUpdateNoteViewState extends State<CreateUpdateNoteView> {
     _textController.addListener(_textControllerListener);
   }
 
-  Future<DatabaseNote> createOrGetExistingNote(BuildContext context) async {
-    final widgetNote = context.getArgument<DatabaseNote>();
+  Future<CloudNote> createOrGetExistingNote(BuildContext context) async {
+    final widgetNote = context.getArgument<CloudNote>();
 
     if (widgetNote != null) {
       _note = widgetNote;
@@ -58,9 +59,9 @@ class _CreateUpdateNoteViewState extends State<CreateUpdateNoteView> {
     }
 
     final currentUser = AuthService.firebase().currentUser!;
-    final email = currentUser.email;
-    final owner = await _notesService.getUser(email: email);
-    final newNote = await _notesService.createNote(owner: owner);
+    final userId = currentUser.id;
+
+    final newNote = await _notesService.createNote(ownerUserId: userId);
     _note = newNote;
 
     return newNote;
@@ -70,7 +71,9 @@ class _CreateUpdateNoteViewState extends State<CreateUpdateNoteView> {
     final note = _note;
 
     if (_textController.text.isEmpty && note != null) {
-      _notesService.deleteNote(id: note.id);
+      _notesService.deleteNote(
+        documentId: note.documentId,
+      );
     }
   }
 
@@ -80,7 +83,7 @@ class _CreateUpdateNoteViewState extends State<CreateUpdateNoteView> {
 
     if (note != null && text.isNotEmpty) {
       await _notesService.updateNote(
-        note: note,
+        documentId: note.documentId,
         text: text,
       );
     }
--- lib/views/notes/notes_list_view.dart
+++ lib/views/notes/notes_list_view.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:vandad_course_app/services/crud/notes_service.dart';
+import 'package:vandad_course_app/services/cloud/cloud_note.dart';
 import 'package:vandad_course_app/utilities/dialogs/delete_dialog.dart';
 
-typedef NoteCallback = void Function(DatabaseNote note);
+typedef NoteCallback = void Function(CloudNote note);
 
 class NotesListView extends StatelessWidget {
   const NotesListView({
@@ -14,14 +14,14 @@ class NotesListView extends StatelessWidget {
 
   final NoteCallback onTap;
   final NoteCallback onDeleteNote;
-  final List<DatabaseNote> notes;
+  final Iterable<CloudNote> notes;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: notes.length,
       itemBuilder: (context, i) {
-        final note = notes[i];
+        final note = notes.elementAt(i);
         return ListTile(
           onTap: () {
             onTap(note);
